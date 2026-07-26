75b4f31ab4dcf16db99a72e75d4a5bc5fbc24171
Advay Welling <advaywelling@gmail.com>
2026-07-26 12:04:35 -0500

    instr

--- main.cpp
+++ main.cpp
@@ -6,9 +6,33 @@
 constexpr size_t WAVE_SIZE = 8;
 constexpr int num_regs = 4;
 using Value = uint32_t;
+using reg_file = std::vector<std::array<Value, WAVE_SIZE>>;
 
-void dump_regs(const std::vector<std::array<Value, WAVE_SIZE>>& regs) {
-    for(int i{}; i < num_regs; i++) {
+enum class Opcode : uint8_t {
+    MOV_IMM_U32, // rd = imm
+    ADD_U32, // rd = ra + ab
+};
+
+struct Operand {
+    enum class Kind : uint8_t {
+        Reg,
+        Imm,
+    };
+    Kind kind;
+    Value value; // reg -> reg idx, imm -> the bits
+};
+
+inline constexpr uint16_t NO_GUARD = 0xFFFF;
+
+struct Instruction {
+    Opcode op;
+    std::array<Operand, 3> operands;
+    uint16_t guard = NO_GUARD; // predicate reg idx
+    bool guard_negate = false;
+};
+
+void dump_regs(const reg_file& regs) {
+    for(size_t i{}; i < regs.size(); i++) {
         std::cout << "r" << i << ":";
         for(size_t j{}; j < WAVE_SIZE; j++) {
             std::cout << " " << regs[i][j];
@@ -18,9 +42,9 @@ void dump_regs(const std::vector<std::array<Value, WAVE_SIZE>>& regs) {
 }
 
 int main() {
-    std::vector<std::array<Value, WAVE_SIZE>> regs(num_regs);
+    reg_file regs(num_regs);
     for(size_t i{}; i < WAVE_SIZE; i++) {
         regs[0][i] = i;
     }
     dump_regs(regs);
- }
+}
