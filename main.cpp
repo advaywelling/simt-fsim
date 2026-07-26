8decd1572e8ed276df69d56fead0c1c989e2cdbc
Advay Welling <advaywelling@gmail.com>
2026-07-26 12:39:52 -0500

    the add is working da

--- main.cpp
+++ main.cpp
@@ -39,12 +39,42 @@ void dump_regs(const reg_file& regs) {
         }
         std::cout << "\n";
     }
+
+    std::cout << " ---------- " << "\n";
 }
 
 int main() {
     reg_file regs(num_regs);
-    for(size_t i{}; i < WAVE_SIZE; i++) {
-        regs[0][i] = i;
+    for (size_t i{}; i < regs.size(); i++) {
+        for (size_t j{}; j < WAVE_SIZE; j++) {
+            regs[i][j] = i + j;
+        }
     }
     dump_regs(regs);
+    Operand dest = {Operand::Kind::Reg, 0};
+    Operand src1 = {Operand::Kind::Reg, 1};
+    Operand src2 = {Operand::Kind::Reg, 2};
+    std::array<Operand, 3> operands = {dest, src1, src2};
+    Instruction instr = {Opcode::ADD_U32, operands};
+    switch (instr.op) {
+        case Opcode::MOV_IMM_U32 : {
+            for(size_t i{}; i < WAVE_SIZE; i++) {
+                regs[instr.operands[0].value][i] = instr.operands[1].value;
+            }
+            break;
+        }
+        case Opcode::ADD_U32 : {
+            for(size_t i{}; i < WAVE_SIZE; i++) {
+                Value val1 = (instr.operands[1].kind == Operand::Kind::Reg) ? regs[instr.operands[1].value][i] : instr.operands[1].value;
+                Value val2 = (instr.operands[2].kind == Operand::Kind::Reg) ? regs[instr.operands[2].value][i] : instr.operands[2].value;
+                regs[instr.operands[0].value][i] = val1 + val2;
+                dump_regs(regs);
+            }
+            break;
+        }
+        default:
+            break;
+    }
+
+    //dump_regs(regs);
 }
