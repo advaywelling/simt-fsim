d008ab7df9fedb795de30b4d7db6053086f4fae1
Advay Welling <advaywelling@gmail.com>
2026-08-16 01:05:45 -0400

    smol fix

--- src/wave.h
+++ src/wave.h
@@ -22,7 +22,7 @@ class Wave {
     private:
         size_t pc {};
         RegFile regs;
-        std::vector<uint32_t> gmem;
+        std::vector<uint32_t> gmem; // yes i know gmem is NOT per wave but ill change this sometime
         uint32_t resolve(const Operand& op, size_t lane) const;
         std::array<bool, WAVE_SIZE> active_mask; // who's active rn
         std::vector<ReconvEntry> simt_stack; // all deferred lanes
